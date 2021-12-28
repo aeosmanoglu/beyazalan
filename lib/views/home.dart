0de7b153d9c9ccb651297ff3bb178b8c35441a24
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2021-12-28 23:52:50 +0300

    feat: home

---
+++ lib/views/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Beyaz Alan'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.manage_accounts_outlined),
+              onPressed: () {
+                null;
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Text(
+              'Bilgi almak istediğiniz cihazın QR kodunu okutun.',
+              style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                    color: Colors.grey,
+                  ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            null;
+          },
+          child: const Icon(Icons.qr_code),
+        ),
+      );
+}
